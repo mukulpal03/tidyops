a08725fe18357bca4c17f46633995b38ed6742cd
Mukul Pal <mukulpal0333@gmail.com>
2025-06-28 18:21:13 +0530

    updated the validation schema

--- store/dataStore.ts
+++ store/dataStore.ts
@@ -1,17 +1,29 @@
-import { ClientData, DataStore, TaskData, WorkerData } from "@/types";
+import {
+  CellError,
+  ClientData,
+  DataStore,
+  TaskData,
+  WorkerData,
+} from "@/types";
 import { create } from "zustand";
 
 export const useDataStore = create<DataStore>((set) => ({
   clients: [],
   workers: [],
   tasks: [],
-  errors: null,
+  errors: [],
+  cellErrors: [],
 
   setClients: (data: ClientData[]) => set({ clients: data }),
-
   setWorkers: (data: WorkerData[]) => set({ workers: data }),
-
   setTasks: (data: TaskData[]) => set({ tasks: data }),
 
-  setErrors: (data: string) => set({ errors: data }),
+  setErrors: (newErrors: string[]) => set({ errors: newErrors }),
+  addError: (errorMsg: string) =>
+    set((state) => ({ errors: [...state.errors, errorMsg] })),
+  clearErrors: () => set({ errors: [] }),
+
+  setCellErrors: (newCellErrors: CellError[]) =>
+    set({ cellErrors: newCellErrors }),
+  clearCellErrors: () => set({ cellErrors: [] }),
 }));
